85412a87dc4f23c01f151c4e4d7cf85b37a5ecfe
Jesica Halife <jesica.halife@aerolab.co>
2022-07-11 19:48:09 -0300

    vercel json

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,21 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   
   const timestamp = +new Date()
   console.log("hello from hello", timestamp)
-  fetch(`https://functions-test-two.vercel.app/api/goodbye`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ timestamp }),
-  })
-  setTimeout(() => {
-    return res.status(200).json({
-      status: "bye"
-    })
-  }, 1000)
-  
+  res.statusCode = 200
+  res.end()
+  console.log("Start async")
+  await test()
+
 }
 
+const test = async () => {
+  setTimeout(() => {
+    console.log("my execution")
+  }, 10000)
+}
