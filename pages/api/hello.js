7007dc40616c32dd08d4f992b3d0d7e98d02931c
Jesica Halife <jesica.halife@aerolab.co>
2022-07-12 10:06:39 -0300

    handle error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,19 +4,24 @@ import Mergent from 'mergent'
 // could be some API route / getServerSideProps / ...
 async function handler (req, res)  {
     const mergent = new Mergent("A4XaV2S3GQbxnQvVswCC");
-    const task = await mergent.tasks
-    .create({
-      request: {
-        headers: { "Content-Type": "application/json" }, // Optional
-        url: "https://functions-test-two.vercel.app/api/goodbye",
-        body: JSON.stringify({
-            name: "John Doe",
-            age: "42"
-        }) // Optional
-      },
-      delay: "PT5S"
-    })
-   console.log({task})
+    try {
+        const task = await mergent.tasks
+        .create({
+          request: {
+            headers: { "Content-Type": "application/json" }, // Optional
+            url: "https://functions-test-two.vercel.app/api/goodbye",
+            body: JSON.stringify({
+                name: "John Doe",
+                age: "42"
+            }) // Optional
+          },
+          delay: "PT5S"
+        })
+       console.log({task})
+    }catch(err) {
+        console.log(err)
+    }
+    
   res.status(200).send("");
 }
 export default handler
